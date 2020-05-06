272fa7d562e05e7920d5c408c5eda9fe71582996
EmilLundstedt <sti.emil.lundstedt@gmail.com>
2020-05-06 14:55:13 +0200

    Changed header, added signup form, fixed main popup window, indented code.

--- js/my_jquery.js
+++ js/my_jquery.js
@@ -2,29 +2,33 @@
 $(document).ready(function(){
 
 
-    $(window).resize(function(){
-      if ($(".burger_row").css("background-color")!="rgb(255, 255, 255)") $(".burger_row").toggleClass("changebackground") && $(".main-section header").toggleClass("click");;
+  $(window).resize(function(){
+    if ($(".burger_row").css("background-color")!="rgb(255, 255, 255)") $(".burger_row").toggleClass("changebackground") && $(".main-section header").toggleClass("click");;
 
-      $(".main-section .nav-links").hide();
+    $(".main-section .nav-links").hide();
   })
 
-    $('.hamburger').click(function() {
-         $(".main-section header").toggleClass("click");
-        $(".main-section .nav-links").fadeToggle(200);
-        $(".burger_row").toggleClass("changebackground");
-    });
+  $('.hamburger').click(function() {
+   $(".main-section header").toggleClass("click");
+   $(".main-section .nav-links").fadeToggle(200);
+   $(".burger_row").toggleClass("changebackground");
+ });
 
 });
+
+$('.button').click(function(){
+ window.location.href='signup.html';
+})
 
 
 
 $(function () {
-    count = 0;
-    countryArray = ["China", "Greece", "India", "Jamaica", "Japan", "Korea", "Honduras", "Haiti", "Nepal"];
-    setInterval(function () {
-        count++;
-        $("#word").fadeOut(1000, function () {
-          $(this).text(countryArray[count % countryArray.length]).fadeIn(1000);
-      });
-    }, 5000);
+  count = 0;
+  countryArray = ["China", "Greece", "India", "Jamaica", "Japan", "Korea", "Honduras", "Haiti", "Nepal"];
+  setInterval(function () {
+    count++;
+    $("#word").fadeOut(1000, function () {
+      $(this).text(countryArray[count % countryArray.length]).fadeIn(1000);
+    });
+  }, 5000);
 });
